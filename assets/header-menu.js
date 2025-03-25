16fe5c948436fe19f5070a23d55d8d7286b64f03
Bertin Kenol <bertkenol@gmail.com>
2025-03-24 22:13:43 -0400

    Updated to Latest "Dawn" Theme
    * Updated to latest "Dawn" theme in Shopify
    *Fixed header menu going away when mouse moved away

--- assets/header-menu.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.mega-menu').forEach((menu) => {
-    menu.addEventListener('mouseenter', () => {
-      menu.setAttribute('open', true);
-    });
-  
-    menu.addEventListener('mouseleave', () => {
-      menu.removeAttribute('open');
-    });
-  });
